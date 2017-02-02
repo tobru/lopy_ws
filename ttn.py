d27d3e0d2118c63dc77dc4a3eba0aa2ca1e79ba2
cfaessler <cfaessle@hsr.ch>
2017-02-02 16:36:34 +0100

    Initial commit

---
+++ ttn.py
@@ -0,0 +1,36 @@
+from network import LoRa
+import socket
+import binascii
+import struct
+
+# Initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify('26011057'))[0]
+nwk_swkey = binascii.unhexlify('C0806726D5560CAAC21780F2CE73CAEE')
+app_swkey = binascii.unhexlify('67C7EDF2726530BC4EBAD996EC54C803')
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+
+# make the socket blocking
+# (waits for the data to be sent and for the 2 receive windows to expire)
+s.setblocking(True)
+
+# send some data
+s.send(bytes([0x01, 0x02, 0x03]))
+
+# make the socket non-blocking
+# (because if there's no data received it will block forever...)
+s.setblocking(False)
+
+# get any data received (if any...)
+data = s.recv(64)
+print(data)
